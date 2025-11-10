f8e54dd8af992b656cdabcfdbe6b6251e13ffadd
The Hustle CentreZW <thehustlecentrezw@gmail.com>
2025-11-10 01:53:36 +0000

    Firebase: error (auth/configuration-not-found)
    STILL!!!

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,11 +5,10 @@ import { getFirestore, enableIndexedDbPersistence } from 'firebase/firestore';
 const firebaseConfig = {
   projectId: 'studio-2244406487-aef62',
   appId: '1:522660406159:web:09fb418090af2f84a5a1fa',
+  storageBucket: 'studio-2244406487-aef62.appspot.com',
   apiKey: 'AIzaSyCPEV6oHXAwOqvmuc-0vdRMeZ2OXQixJoI',
   authDomain: 'studio-2244406487-aef62.firebaseapp.com',
-  measurementId: '',
   messagingSenderId: '522660406159',
-  storageBucket: 'studio-2244406487-aef62.appspot.com'
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
